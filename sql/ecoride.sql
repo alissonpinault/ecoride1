c787cba171d68d735c6f2d04489136c06e294e1b
ohlafolle <pinault.alisson@gmail.com>
2025-07-21 20:18:11 +0200

    sql

--- sql/ecoride.sql
+++ sql/ecoride.sql
@@ -1,29 +1,176 @@
-CREATE DATABASE IF NOT EXISTS ecoride CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE ecoride+;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 21 juil. 2025 à 20:15
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    pseudo VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('utilisateur', 'employe', 'admin') DEFAULT 'utilisateur',
-    credits INT DEFAULT 20,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE covoiturages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    conducteur_id INT NOT NULL,
-    ville_depart VARCHAR(100) NOT NULL,
-    ville_arrivee VARCHAR(100) NOT NULL,
-    date_depart DATE NOT NULL,
-    heure_depart TIME NOT NULL,
-    duree INT NOT NULL, -- en minutes
-    nb_places INT NOT NULL,
-    prix DECIMAL(5,2) NOT NULL,
-    vehicule VARCHAR(100),
-    est_ecologique BOOLEAN DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (conducteur_id) REFERENCES users(id) ON DELETE CASCADE
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `ecoride`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `avis_id` int(11) NOT NULL,
+  `commentaire` varchar(50) NOT NULL,
+  `note` varchar(50) NOT NULL,
+  `statut` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `covoiturage`
+--
+
+CREATE TABLE `covoiturage` (
+  `covoiturage_id` int(11) NOT NULL,
+  `date_depart` date NOT NULL,
+  `heure_depart` date NOT NULL,
+  `lieu_depart` varchar(50) NOT NULL,
+  `date_arrivee` date NOT NULL,
+  `heure_arrivee` varchar(50) NOT NULL,
+  `lieu_arrivee` varchar(50) NOT NULL,
+  `statut` varchar(50) NOT NULL,
+  `nb_place` int(50) NOT NULL,
+  `prix_personne` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('utilisateur','employe','admin') DEFAULT 'utilisateur',
+  `credits` int(11) DEFAULT 20,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `utilisateur_id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `telephone` varchar(50) NOT NULL,
+  `adresse` varchar(50) NOT NULL,
+  `date_naissance` varchar(50) NOT NULL,
+  `photo` blob NOT NULL,
+  `pseudo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`utilisateur_id`, `nom`, `prenom`, `email`, `password`, `telephone`, `adresse`, `date_naissance`, `photo`, `pseudo`) VALUES
+(1, 'jean', 'paul', 'jean@paul.com', 'ouijean', '06.32.36.35.56', 'allée des pigeons', '01/02/1958', '', 'jean_paul'),
+(2, 'marie', 'jeanne', 'marie@jeanne', 'ouimarie', '06.32.36.35.50', 'allée des singes', '01/03/1923', '', 'marie_jeanne');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voiture`
+--
+
+CREATE TABLE `voiture` (
+  `voiture_id` int(11) DEFAULT NULL,
+  `modele` varchar(50) NOT NULL,
+  `immatriculation` varchar(50) NOT NULL,
+  `energie` varchar(50) NOT NULL,
+  `couleur` varchar(50) NOT NULL,
+  `date_premiere_immatriculation` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD PRIMARY KEY (`avis_id`);
+
+--
+-- Index pour la table `covoiturage`
+--
+ALTER TABLE `covoiturage`
+  ADD PRIMARY KEY (`covoiturage_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`utilisateur_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `email_2` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `avis`
+--
+ALTER TABLE `avis`
+  MODIFY `avis_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `covoiturage`
+--
+ALTER TABLE `covoiturage`
+  MODIFY `covoiturage_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `utilisateur_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
